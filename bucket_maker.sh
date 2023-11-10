98010cef527a0aa835aaf356aa7c60b410f67fe4
Rachel Ring <rachelring84@gmail.com>
2023-11-10 11:13:32 +0000

    Add files via upload
    This script was created for our first continuous assessment in which I received an A

---
+++ bucket_maker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+count=0
+
+if [ $# -ne 1 ]; then
+	echo "This script needs one bucket name only"
+	echo "Usage: ./bucket_maker.sh [BUCKET_NAME]"
+	exit 1
+fi
+
+gsutil mb gs://$1 2>/dev/null
+if [ $? -eq 0 ]; then
+	echo "Bucket $1 was created succesully"
+	exit 0
+else
+	echo "failed to create bucket $1"
+fi
+
+for i in {1..2}; do
+	gsutil mb gs://$1$i 2>/dev/null
+	if [ $? -eq 0 ]; then
+		echo "Bucket $1$i was sucessfully created"
+		exit 0
+	else
+		echo "failed to create bucket $1$i"
+	fi
+done
+
+echo "Cannot create bucket"
+echo "Finished"
